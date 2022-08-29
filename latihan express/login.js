aa1e332d84356ce7badee6fce01a5e3c9fb0aae8
syahrullabdillah <syahrullabdillah12@gmail.com>
2022-08-29 19:38:29 +0700

    pertama

---
+++ latihan express/login.js
@@ -0,0 +1,22 @@
+const express = require('express') //include Expressjs
+const app = express() //create an Expressjs app
+const bodyParser = require('body-parser'); //midleware
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+//reute to homepage
+app.get('/',(req, res) => {
+    res.sendFIle(__dirname + '/login.html');
+});
+
+app.post('/login', (req, req) => {
+    //Insert Login Code Here
+    let username = req.body.username;
+    let password = req.body.password;
+    res.send(`Username: ${username} Password: ${password}`);
+});
+
+const port = 3000 //port we will listen on
+
+//function to listen on the port
+app.listen(port, () => console.log(`This app is listening on port ${port}`));
